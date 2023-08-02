bb64ad3217f67e1d2aea707b3b97f5e35b1749ff
IshanShino <ishans1045@gmail.com>
2023-08-03 00:32:55 +0530

    Setting up Enemy AI. Adding a gun and crosshair.  Raycast intro.

--- Unnamed FPS Project/Assets/Scripts/EnemyAI.cs
+++ Unnamed FPS Project/Assets/Scripts/EnemyAI.cs
@@ -7,16 +7,54 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
     NavMeshAgent navMeshAgent;
     void Start()
-    {
+    {   
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
+    {   
+        distanceToTarget =  Vector3.Distance(target.position, transform.position);
+
+        if (isProvoked == true)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    void ChaseTarget()
     {
         navMeshAgent.SetDestination(target.position);
+    }
+    void AttackTarget()
+    {
+        Debug.Log("You were hit!");
+    }
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 
 }
